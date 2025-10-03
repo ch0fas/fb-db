ee0a97ed741653ddd7735766ad2654b07a91b01a
ch0fas <sof.mal.gar55@gmail.com>
2025-10-02 19:09:01 -0600

    finished get_team_record for now

--- team_tools.go
+++ team_tools.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
-
 	"github.com/gocolly/colly"
 )
 
@@ -25,7 +24,7 @@ var TEAMS_MAP = map[string]string{
 	"sdg": "Los Angeles Chargers", //ADD NAME CHANGE IF SAN DIEGO
 	"den": "Denver Broncos",
 	"kan": "Kansas City Chiefs",
-	"rai": "Las Vegas/Oakland Raiders",
+	"rai": "Las Vegas Raiders",
 	"phi": "Philadelphia Eagles",
 	"was": "Washington Commanders",
 	"dal": "Dallas Cowboys",
@@ -86,6 +85,46 @@ func get_team_record(team string, year string) []string {
 				result = append(result, "Los Angeles Chargers")
 			} else {
 				result = append(result, "San Diego Chargers")
+			}
+		case "rai":
+			if numeric_year > 2019 {
+				result = append(result, "Las Vegas Raiders")
+			} else if (numeric_year < 1995) && (numeric_year > 1981) {
+				result = append(result, "Los Angeles Raiders")
+			} else {
+				result = append(result, "Oakland Raiders")
+			}
+		case "was":
+			if numeric_year > 2021 {
+				result = append(result, "Washington Commanders")
+			} else if (numeric_year == 2021) || (numeric_year == 2020) {
+				result = append(result, "Washington Football Team")
+			} else if (numeric_year < 2020) && (numeric_year > 1936) {
+				result = append(result, "Washington Redskins")
+			} else if (numeric_year < 1937) && (numeric_year > 1932) {
+				result = append(result, "Boston Redskins")
+			} else {
+				result = append(result, "Boston Braves")
+			}
+		case "ram":
+			if (numeric_year > 2016) || (numeric_year > 1945 && numeric_year < 1995) {
+				result = append(result, "Los Angeles Rams")
+			} else if (numeric_year > 1994) && (numeric_year < 2016) {
+				result = append(result, "St. Louis Rams")
+			} else {
+				result = append(result, "Cleveland Rams")
+			}
+		case "crd":
+			if (numeric_year > 1993) {
+				result = append(result, "Arizona Cardinals")
+			} else if (numeric_year < 1994) && (numeric_year > 1987) {
+				result = append(result, "Phoenix Cardinals")
+			} else if (numeric_year < 1988) && (numeric_year > 1959) {
+				result = append(result, "St. Louis Cardinals")
+			} else if (numeric_year == 1944) {
+				result = append(result, "Chi/Pit Cards/Steelers")
+			} else {
+				result = append(result, "Chicago Cardinals")
 			}
 		default:
 			result = append(result, TEAMS_MAP[team])
